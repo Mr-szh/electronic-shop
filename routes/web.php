fbef4186db1a165e1df55e6eb97ef84ec1b3a023
vagrant <vagrant@homestead>
2019-07-31 12:26:17 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+// 在之前的路由后面配上中间件
+// Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 Route::get('/', 'PagesController@root')->name('root');
-Auth::routes();
+// 在之前的路由里加上一个 verify 参数
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
